bf7dd2278d076ae6c6f9ab572e282c401ba51af7
shadowfeind <travestroy007@gmail.com>
2024-04-22 12:45:52 +0545

    starting project

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+export const Dashboardlayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div>
+      <h1>layout</h1>
+      {children}
+    </div>
+  );
+};
+export default Dashboardlayout;
